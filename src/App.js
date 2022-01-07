2fef1292217fe8df64a59c1e3793f3ec467688aa
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-07 11:52:40 -0500

    :construction: added routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <header>
-        
+        <Link to="/software">Software</Link> | {" "}
+        <Link to="/support">Support</Link> | {" "}
+        <Link to="/order">Order</Link>
       </header>
     </div>
   );
