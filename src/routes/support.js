724dc3df040a8aa19005fc141060b2ddd20e6295
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-07 12:10:51 -0500

    :construction: basic info in support page

--- src/routes/support.js
+++ src/routes/support.js
@@ -1,7 +1,12 @@
+import { Card } from 'react-bootstrap';
+
 export default function Support() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Support</h2>
+        <Card>
+            Email: _david's email but obfuscated_
+        </Card>
       </main>
     );
   }
