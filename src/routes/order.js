1905fa2213b3f94f6ad939b13aecef2d09c8795c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-08 18:38:47 -0500

    :construction: added mock items to order page

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,21 +1,57 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 
 export default function Order() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <Container>
-            <Row>
-                <Col style={{textAlign: "left"}}>
-                    Window Inverter 
-                </Col>
-                <Col md="auto">
-                    <Button>Add To Cart</Button>
-                </Col>
-                <Col md="auto">
-                    <Button>Buy Now</Button>
-                </Col>
-            </Row>
+        <h2 className="align-items-center">Order</h2>
+        <Container className="text-left">
+            <Stack gap={3}>
+                <Row>
+                    <Col style={{textAlign: "left"}}>
+                        <h3>Window Inverter</h3> 
+                    </Col>
+                    <Col md="auto">
+                        <Button>Add To Cart</Button>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Buy Now</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{textAlign: "left"}}>
+                        <h3>Echidna Salivation Dessicator</h3>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Add To Cart</Button>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Buy Now</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{textAlign: "left"}}>
+                        <h3>Chicken Chow Mein Prognositicator</h3>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Add To Cart</Button>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Buy Now</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col style={{textAlign: "left"}}>
+                        <h3>Snickers Dick Vein Sclerotherapy Lazer</h3>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Add To Cart</Button>
+                    </Col>
+                    <Col md="auto">
+                        <Button>Buy Now</Button>
+                    </Col>
+                </Row>
+            </Stack>
         </Container>
       </main>
     );
-  }
+}
