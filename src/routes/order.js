360d52b60ece97c56a5ce8aad92b784dfe6f3612
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-08 16:46:46 -0500

    :construction: added basic skeleton for order page

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,7 +1,21 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+
 export default function Order() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2>Order</h2>
+        <Container>
+            <Row>
+                <Col style={{textAlign: "left"}}>
+                    Window Inverter 
+                </Col>
+                <Col md="auto">
+                    <Button>Add To Cart</Button>
+                </Col>
+                <Col md="auto">
+                    <Button>Buy Now</Button>
+                </Col>
+            </Row>
+        </Container>
       </main>
     );
   }
