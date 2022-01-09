704fe441c1eaf784c5ee2d05e3d47f935d44dead
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-09 15:45:25 -0500

    :construction: Fixed responsive styling

--- src/routes/order.js
+++ src/routes/order.js
@@ -3,50 +3,50 @@ import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 export default function Order() {
     return (
       <main style={{ padding: "1rem 0" }}>
-        <h2 className="align-items-center">Order</h2>
-        <Container className="text-left">
+        <Container>
+        <h2 className="text-md-start text-center">Order</h2>
             <Stack gap={3}>
                 <Row>
-                    <Col style={{textAlign: "left"}}>
+                    <Col className="text-center text-sm-center text-md-start">
                         <h3>Window Inverter</h3> 
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Add To Cart</Button>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Buy Now</Button>
                     </Col>
                 </Row>
                 <Row>
-                    <Col style={{textAlign: "left"}}>
+                    <Col className="text-center text-sm-center text-md-start">
                         <h3>Echidna Salivation Dessicator</h3>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Add To Cart</Button>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Buy Now</Button>
                     </Col>
                 </Row>
                 <Row>
-                    <Col style={{textAlign: "left"}}>
+                    <Col className="text-center text-sm-center text-md-start">
                         <h3>Chicken Chow Mein Prognositicator</h3>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Add To Cart</Button>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Buy Now</Button>
                     </Col>
                 </Row>
                 <Row>
-                    <Col style={{textAlign: "left"}}>
+                    <Col className="text-center text-sm-center text-md-start">
                         <h3>Snickers Dick Vein Sclerotherapy Lazer</h3>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Add To Cart</Button>
                     </Col>
-                    <Col md="auto">
+                    <Col className="text-center text-sm-center text-md-start" md="auto">
                         <Button>Buy Now</Button>
                     </Col>
                 </Row>
