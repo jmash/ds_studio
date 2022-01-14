3d8f9738d22da0f2c1d47fe2f8c65883b9dd1882
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-14 11:47:45 -0500

    :construction: add effect to set dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import { Outlet, Link } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 import SiteNavbar from './ui/navbar';
+import { useEffect } from 'react';
+
+
 
 function App() {
+  
+  useEffect(() => {
+    document.body.classList.add('bg-dark', 'text-light');
+
+    return () => {
+      document.body.classList.remove('bg-black', 'text-light');
+    }
+  })
+
   return (
     <>
+      <h1 className="text-center App-header"><Link to="/"><del>Allen St</del> Software</Link></h1>
+      <SiteNavbar />
       <Container>
-        <h1 className="text-center"><Link to="/"><del>Allen St</del> Software</Link></h1>
-        <SiteNavbar />      
         <Outlet />
       </Container>
     </>
