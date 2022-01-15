eddd91b210624cdca8be5dc346c88a9ee798c866
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-15 14:47:20 -0500

    :lipstick: remove decoration from navbar links

--- src/ui/SiteNavbar/Sitenavbar.js
+++ src/ui/SiteNavbar/Sitenavbar.js
@@ -5,20 +5,20 @@ import siteNavbarStyles from './SiteNavbar.module.css';
 
 export default function SiteNavbar() {
     return (
-        <Nav className={ cx(siteNavbarStyles.Sitenavbar, "justify-content-center text-center")}>
+        <Nav className={ cx(siteNavbarStyles.SiteNavbar, "justify-content-center text-center")}>
           <NavDropdown title="Software" id="nav-software">
             <NavDropdown.Item eventKey="1.1">
-              <Link to="/productdetails">Window Inverter</Link>
+              <Link className="text-decoration-none" to="/productdetails">Window Inverter</Link>
             </NavDropdown.Item>
           </NavDropdown>
           <Nav.Item>
             <Nav.Link eventKey="2">
-              <Link to="/support">Support</Link>
+              <Link className="text-decoration-none" to="/support">Support</Link>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link eventKey="3">
-              <Link to="/order">Order</Link>
+              <Link className="text-decoration-none" to="/order">Order</Link>
             </Nav.Link>
           </Nav.Item>
         </Nav>
