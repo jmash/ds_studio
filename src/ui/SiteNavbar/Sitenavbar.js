7391819b52dbac4a495054e942a5513e47c588f2
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-17 02:14:20 -0500

    :lipstick: added borders to site navbar

--- src/ui/SiteNavbar/Sitenavbar.js
+++ src/ui/SiteNavbar/Sitenavbar.js
@@ -6,17 +6,17 @@ import siteNavbarStyles from './SiteNavbar.module.css';
 export default function SiteNavbar() {
     return (
         <Nav className={ cx(siteNavbarStyles.SiteNavbar, "justify-content-center text-center")}>
-          <NavDropdown title="Software" id="nav-software">
+          <NavDropdown className="border-start border-end" title="Software" id="nav-software">
             <NavDropdown.Item eventKey="1.1">
               <Link className="text-decoration-none" to="/productdetails">Window Inverter</Link>
             </NavDropdown.Item>
           </NavDropdown>
-          <Nav.Item>
+          <Nav.Item className="">
             <Nav.Link eventKey="2">
               <Link className="text-decoration-none" to="/support">Support</Link>
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item>
+          <Nav.Item className="border-start border-end">
             <Nav.Link eventKey="3">
               <Link className="text-decoration-none" to="/order">Order</Link>
             </Nav.Link>
