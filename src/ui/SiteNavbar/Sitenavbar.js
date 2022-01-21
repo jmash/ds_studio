78296e8308b22badb42d4974325655eee0f8f19a
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-21 13:02:45 -0500

    :lipstick: made no underline global using SCSS

--- src/ui/SiteNavbar/Sitenavbar.js
+++ src/ui/SiteNavbar/Sitenavbar.js
@@ -5,20 +5,20 @@ import siteNavbarStyles from './SiteNavbar.module.css';
 
 export default function SiteNavbar() {
     return (
-        <Nav className={ cx(siteNavbarStyles.SiteNavbar, "justify-content-center text-center")}>
+        <Nav className={ cx(siteNavbarStyles.SiteNavbar, "shadow-sm justify-content-center text-center")}>
           <NavDropdown className="border-start border-end" title="Software" id="nav-software">
             <NavDropdown.Item eventKey="1.1">
-              <Link className="text-decoration-none" to="/productdetails">Window Inverter</Link>
+              <Link to="/productdetails">Window Inverter</Link>
             </NavDropdown.Item>
           </NavDropdown>
           <Nav.Item className="">
             <Nav.Link eventKey="2">
-              <Link className="text-decoration-none" to="/support">Support</Link>
+              <Link to="/support">Support</Link>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item className="border-start border-end">
             <Nav.Link eventKey="3">
-              <Link className="text-decoration-none" to="/order">Order</Link>
+              <Link to="/order">Order</Link>
             </Nav.Link>
           </Nav.Item>
         </Nav>
