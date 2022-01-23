9fe99203bb6177cd143a683b0b60a1ea384ae992
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-22 20:08:53 -0500

    :lipstick: nav buttons fully linked

--- src/ui/SiteNavbar/Sitenavbar.js
+++ src/ui/SiteNavbar/Sitenavbar.js
@@ -6,18 +6,18 @@ import siteNavbarStyles from './SiteNavbar.module.css';
 export default function SiteNavbar() {
     return (
         <Nav className={ cx(siteNavbarStyles.SiteNavbar, "shadow-sm justify-content-center text-center")}>
-          <NavDropdown className="dropdown-dark border-start border-end" title="Software" id="nav-software">
+          <NavDropdown variant="dark" className="border-start border-end" title="Software" id="nav-software">
             <NavDropdown.Item eventKey="1.1">
               <Link to="/productdetails">Window Inverter</Link>
             </NavDropdown.Item>
           </NavDropdown>
           <Nav.Item className="">
-            <Nav.Link eventKey="2">
+            <Nav.Link as={Link} to="/support" eventKey="2">
               <Link to="/support">Support</Link>
             </Nav.Link>
           </Nav.Item>
           <Nav.Item className="border-start border-end">
-            <Nav.Link eventKey="3">
+            <Nav.Link as={Link} to="/order" eventKey="3">
               <Link to="/order">Order</Link>
             </Nav.Link>
           </Nav.Item>
