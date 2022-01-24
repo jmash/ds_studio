d5e5f1c3c2292b9ce9002054176da721d5960524
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-01-24 00:49:58 -0500

    :lipstick: :construction: still working on navbar

--- src/ui/SiteNavbar/Sitenavbar.js
+++ src/ui/SiteNavbar/Sitenavbar.js
@@ -1,11 +1,12 @@
 import { Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import cx from 'classnames';
 import siteNavbarStyles from './SiteNavbar.module.css';
 
 export default function SiteNavbar() {
     return (
-        <Nav className={ cx(siteNavbarStyles.SiteNavbar, "shadow-sm justify-content-center text-center")}>
+        <Navbar bg="dark" variant="dark" className={ cx(siteNavbarStyles.SiteNavbar, "pt-0 pb-0 shadow-sm justify-content-center text-center")}>
           <NavDropdown variant="dark" className="border-start border-end" title="Software" id="nav-software">
             <NavDropdown.Item eventKey="1.1">
               <Link to="/productdetails">Window Inverter</Link>
@@ -21,7 +22,7 @@ export default function SiteNavbar() {
               <Link to="/order">Order</Link>
             </Nav.Link>
           </Nav.Item>
-        </Nav>
+        </Navbar>
     );
 }
 
