a35bcbc02477a3af60f0c5fd50938d287ccc905c
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-05-29 00:47:17 -0400

    :constrution: add login/registration logic

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 const express = require("express");
+const router = express.Router();
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.static('public'));
 fs.readdir(__dirname + "/public/product_carousel", (err, files) => {
     if (err) {
-        console.log(err);
+        console.log(err); // TODO: Change this to proper server error response
     } else {
         files.forEach(file => {
             app.use('/product_carousel', express.static(file));
@@ -62,6 +63,72 @@ app.get("/products/:name", async(req, res) => {
         res.status(500).end();
     }
 });
+
+router.post("/register", validInfo, async (req, res) => { // TODO: Create validInfo middleware
+    const { email, name, password } = req.body;
+
+    try {
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+        if(user.rows.length > 0) {
+            return res.status(401).json("User already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const bcryptPassword = await bcrypt.hash(password, salt);
+
+        let newUser = await pool.query(
+            "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
+            [name, email, bcryptPassword]
+        );
+
+        const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+
+            return res.json({ jwtToken });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+router.post("/login", validInfo, async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+        email
+      ]);
+  
+      if (user.rows.length === 0) {
+        return res.status(401).json("Invalid Credential");
+      }
+  
+      const validPassword = await bcrypt.compare(
+        password,
+        user.rows[0].user_password
+      );
+  
+      if (!validPassword) {
+        return res.status(401).json("Invalid Credential");
+      }
+      const jwtToken = jwtGenerator(user.rows[0].user_id);
+      return res.json({ jwtToken });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+  
+  router.post("/verify", authorize, (req, res) => {
+    try {
+      res.json(true);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+  
+  module.exports = router;
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
