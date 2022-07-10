89142de2443bc5047388e6279865e49a4305c30f
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-07-09 21:45:40 -0400

    :construction: add shopping cart to navbar

--- client/src/ui/SiteNavbar/SiteNavbar.js
+++ client/src/ui/SiteNavbar/SiteNavbar.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Nav, NavDropdown } from 'react-bootstrap';
+import { Dropdown, Nav, NavDropdown } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
+// import siteNavbarStyles from './SiteNavbar.module.css';
 
 export default function SiteNavbar() {
     const [products, setProducts] = useState([]);
@@ -22,25 +24,28 @@ export default function SiteNavbar() {
     }, []);
 
     return (
-        <Navbar className="pt-0 pb-0 shadow-sm justify-content-center text-center">
-          <NavDropdown className="border-start border-end" title="Software" id="nav-software">
-            {products.map(product => (
-              <NavDropdown.Item as="span" eventKey={`1.${product.product_id}`} key={`${product.product_id}`}>
-                <Link to={`/productdetails/${product.product_link}`}>{product.product_name}</Link>
-              </NavDropdown.Item>
-            ))}
-            
-          </NavDropdown>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/support" eventKey="2">
-              <span>Support</span>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="border-start border-end">
-            <Nav.Link as={Link} to="/order" eventKey="3">
-              <span>Order</span>
-            </Nav.Link>
-          </Nav.Item>
+        <Navbar className="navbar pt-0 pb-0 shadow-sm nav-justified text-center">
+          <Nav className="w-100 navbar-expand-md justify-content-center order-1">
+            <NavDropdown className="border-start border-end" title="Software" id="nav-software">
+              {products.map(product => (
+                <Nav.Link eventKey={`1.${product.product_id}`} key={`${product.product_id}`} as={Link} to={`/productdetails/${product.product_link}`}>{product.product_name}</Nav.Link>
+              ))}
+              
+            </NavDropdown>
+            <Nav.Item>
+              <Nav.Link as={Link} to="/support" eventKey="2">
+                <span>Support</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="border-start border-end">
+              <Nav.Link as={Link} to="/order" eventKey="3">
+                <span>Order</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="order-2">
+            <ShoppingCart />
+          </Nav>
         </Navbar>
     );
 }
