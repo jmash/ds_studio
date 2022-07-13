4b0fe7c4bc0290d2354b64f72e645cb8d583e7f0
Jared M Ashcraft <jaredmashcraft@gmail.com>
2022-07-13 11:53:04 -0400

    :construction: Add Register component

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import Order from './routes/Order/Order';
 import Main from './routes/Main/Main';
 import Purchased from './routes/Purchased/Purchased';
 import Login from './routes/Login/Login';
+import Register from './routes/Register/Register';
 import ProductDetails from './routes/ProductDetails/ProductDetails';
 import "../node_modules/@glidejs/glide/dist/css/glide.core.min.css";
 import "../node_modules/@glidejs/glide/dist/css/glide.theme.min.css";
@@ -26,6 +27,7 @@ ReactDOM.render(
         <Route path="order" element={<Order />} />
         <Route path="purchased" element={<Purchased />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
         <Route path="productdetails/:productName" element={<ProductDetails /> } />
       </Route>
     </Routes>
---
+++ client/src/routes/Register/Register.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const Register = props => {
+  const {register, handleSubmit, formState: {errors}, watch} = useForm();
+  // choosing not to use React Bootstrap here in order to simplify implementation of react-hook-form
+
+  return (
+    <>
+        <div className="p-3 mt-3 card">
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit((data) => {
+                console.log(data); // data will get tossed into the endpoint
+            })}>
+                <div className="container w-75">
+                    <div className="row">
+                        <div className="input-group">
+                            <input aria-describedby="email_input_label" aria-label="Enter Email Here" className="p-3 form-control" {...register("email", { 
+                                    required: "* Email is required",
+                                    pattern: "([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])"
+                                })} 
+                                placeholder="Email" type="email"></input>
+                        </div>
+                        <div className="pt-0 pb-1 my-0">
+                            <p className="form-label">{errors.email?.message}</p>
+                        </div>
+                    </div>
+                    <div className="row g-3 py-0 align-items-center">
+                        <div className="input-group">
+                            <input aria-describedby="password_input_label" aria-label="Enter Password Here" className="p-3 form-control"
+                                {...register("password", { 
+                                    required: "* Password is required", 
+                                    minLength: {
+                                        value: 8,
+                                        message: "* Minimum password length is 8"
+                                    }})} placeholder="Password" type="password">
+
+                            </input>
+                        </div>
+                        <div className="pt-0 pb-1 my-0">
+                            {errors.name && errors.name.type === "required" && (
+                                <span role="alert">Password Required</span>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="pb-3 row align-items-center">
+                    <div className="justify-content-center text-center w-100">
+                        <input className="btn btn-block btn-primary border border-end border-start py-3 w-50 fw-bold align-items-center register-submit-btn text-uppercase" type="submit" />
+                    </div>
+                </div>
+            </form>
+        </div>
+        
+    </>
+  )
+}
+
+//([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])"
+
+// Register.propTypes = {};
+
+export default Register;
